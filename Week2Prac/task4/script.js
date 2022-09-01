c26316a74d17305db29f083f871f1ea82e570b2c
Milan Tamang <s222540579@deakin.edu.au>
2022-09-01 16:29:31 +1000

    week2 tasks

---
+++ Week2Prac/task4/script.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext('2d');
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(95,50,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.font = '30px Arial';
+ctx.fillText('Hello World', 10, 50);
